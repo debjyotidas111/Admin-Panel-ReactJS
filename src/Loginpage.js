892668eaa58d109266bf1e4d7383f687c2999fbb
Debjyoti Das <86339364+debjyotidas111@users.noreply.github.com>
2023-06-09 12:52:54 +0530

    Update Loginpage.js

--- src/Loginpage.js
+++ src/Loginpage.js
@@ -8,20 +8,34 @@ export default class Login extends React.Component{
             loginStatus:JSON.parse(localStorage.getItem('loginStatus'))
         }
     }
-    handleSubmit=(e)=>{
+  
+     handleSubmit=(e)=>{
         e.preventDefault();
-        if(e.target[0].value==e.target[1].value){
+        if(e.target[0].value=== 'Eduard Franz' && e.target[1].value === 'jvoxidf09234'){
+            localStorage.setItem('loginStatus',JSON.stringify(true))
+            this.setState({loginStatus:true})
+            return
+        } else if(e.target[0].value=== 'Lara Thayer' && e.target[1].value === '238jf9823j'){
+            localStorage.setItem('loginStatus',JSON.stringify(true))
+            this.setState({loginStatus:true})
+            return
+        } else if(e.target[0].value=== 'Becky George' && e.target[1].value === 'sdijv02394'){
+            localStorage.setItem('loginStatus',JSON.stringify(true))
+            this.setState({loginStatus:true})
+            return
+        } else if(e.target[0].value=== 'Craig Chaney' && e.target[1].value === 'wd92jf09wu'){
             localStorage.setItem('loginStatus',JSON.stringify(true))
             this.setState({loginStatus:true})
             return
         }
-        alert('Incorrect, \n username and password should same ')
+        alert('Incorrect username and password ')
         console.log('user name ',e.target[0].value);
         console.log('password ',e.target[1].value);
         localStorage.setItem('loginStatus',JSON.stringify(false))
         this.setState({loginStatus:false})
 
     }
+    
     render(){
         if(this.state.loginStatus==true){
             return <Redirect to='/'/>
