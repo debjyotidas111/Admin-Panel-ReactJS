ad23603788a7c0a1bd4852c071f532d085409181
Debjyoti Das <86339364+debjyotidas111@users.noreply.github.com>
2023-06-08 23:50:24 -0700

    files pushed
    Admin Panel Project of Edyoda made on ReactJS.

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+export default class Footer extends React.Component{
+    render() {
+        return <div className='footer'>
+            Copyright © 2023 All rights reserved. Debjyoti Das</div>
+    }
+}
